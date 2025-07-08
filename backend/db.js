6b295c086940a3baca15770777a40320417db1dc
Shivam <shivamraghuwanshi741@gmail.com>
2025-07-08 11:47:57 +0530

    env file added

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+require("dotenv").config()
 
-mongoose.connect("mongodb+srv://learnmangodb:2")
+const mongo_uri = process.env.MONGO_URI;
+mongoose.connect(mongo_uri);
 
 const userSchema = new mongoose.userSchema({
     userName: {
